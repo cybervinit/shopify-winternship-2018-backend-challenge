de0e14400c85974c65594e483dcf905e241f4ecb
Vinit Soni <vinitsoni9869@gmail.com>
2017-09-11 00:35:03 -0400

    add page determination

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
 from flask import Flask, jsonify, json
 import requests
+import math
 
 app = Flask(__name__)
 
+API_URL = 'https://backend-challenge-winter-2017.herokuapp.com/customers.json'
+
 @app.route('/')
 def hello_world():
-	r = requests.get('https://backend-challenge-winter-2017.herokuapp.com/customers')
-	hard = r.json()
-	return jsonify(hard)
+	cust_obj = get_api_resp(1)
+	pagination = cust_obj['pagination']
+	get_page_amount(pagination)
+	
+	response_data = {}
+	invalid_customer_list = get_invalid_cust_list(cust_obj)
+	for i in range (1, page_amount):
+		page_number = i+1
+		invalid_customer_list.extend(get_invalid_cust_list(cust_obj))
+		return jsonify(cust_obj)
+		
+	return str(page_amount)
+
+def get_api_resp(page_number):
+	page_query = '?page=%s' % page_number
+	endpoint = API_URL + page_query
+	response = requests.get(endpoint)
+	return response.json()
+
+
+def get_page_amount(pagination):
+	amount = pagination['total'] / float(pagination['per_page'])
+	return int(math.ceil(amount))
+
+
+
+def get_invalid_cust_list(cust_obj):
+	return []
+
+
+@app.route('/full')
+def full():
+	return jsonify(get_api_resp(1))
